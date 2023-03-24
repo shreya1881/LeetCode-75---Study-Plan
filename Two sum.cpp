b1f0c2942175dc75bd91bd6ec7fdf9e2a9861489
Shreya Dutta <85094341+shreya1881@users.noreply.github.com>
2023-03-24 18:48:53 +0530

    Create Two sum.cpp

---
+++ Two sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int>ans;
+       unordered_map<int,int> umap;
+       for(int i=0; i<nums.size(); i++){
+           if(umap.find(target-nums[i])!=umap.end()){
+               ans.push_back(i);
+               ans.push_back(umap[target-nums[i]]);
+           }
+           umap[nums[i]]=i;
+       }
+       return ans;
+    }
+};
